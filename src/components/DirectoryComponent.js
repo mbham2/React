c0fc466d2086690fa3db046991cf444f3d53ccfc
Manjula Bhamidi <m_bhamdi@yahoo.com>
2021-02-01 21:06:08 -0600

    Presentational and Container Components

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,38 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
 
-class Directory extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            
-        };
-    }
-    renderCampsiteInfo(campsite) {
-        if ((this.props.name)===campsite){
-            return(
-                <div className="row"></div>
-            );
-            }
-        
-            return <div/>
-        }
+function RenderDirectoryItem ({campsite, onClick}){
+    return (
+        <Card onClick={()=> onClick(campsite.id)}>
+            <CardImg width="100%" src={campsite.image} alt={campsite.name}/>
+                <CardImgOverlay>
+                    <CardTitle>{campsite.name}</CardTitle>
+                </CardImgOverlay>
+        </Card>
+        );
+}
+    
         
     
-    render(){
-        const directory = this.props.campsites.map(campsite =>{
+    function Directory(props) {
+        const directory = props.campsites.map(campsite =>{
             return (
                 <div key={campsite.id} className="col-md-5 m-1">
-                    <Card onClick={()=> this.props.onClick(campsite.id)}>
-                        <CardImg width="100%" src={campsite.image} alt={campsite.name}/>
-                            <CardImgOverlay>
-                                <CardTitle>{campsite.name}</CardTitle>
-                            </CardImgOverlay>
-                    </Card>
+                    <RenderDirectoryItem campsite={campsite} onClick={props.onClick}/>
                 </div>
             );
         });
+
         return(
             <div className="container">
                 <div className="row">
@@ -41,5 +32,5 @@ class Directory extends Component {
             </div>
         );
     };
-}
+
 export default Directory;
