a5f1fb3e5bc04ef93f6cbb470d3b8611e11e2787
Manjula Bhamidi <m_bhamdi@yahoo.com>
2021-02-18 13:12:37 -0600

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -19,7 +19,7 @@ function RenderCampsite({ campsite }) {
     );
 }
 
-function RenderComments({ comments, addComment, campsiteId }) {
+function RenderComments({ comments, postComment, campsiteId }) {
     if (comments) {
         return (
             <div className="col-md-5 m-1">
@@ -39,7 +39,7 @@ function RenderComments({ comments, addComment, campsiteId }) {
                 }
                 )
                 }
-                <CommentForm campsiteId={campsiteId} addComment={addComment}/>
+                <CommentForm campsiteId={campsiteId} postComment={postComment}/>
             </div>
         )
     }
@@ -88,7 +88,7 @@ function CampsiteInfo(props) {
                     <RenderCampsite campsite={props.campsite} />
                     <RenderComments 
                         comments={props.comments}
-                        addComment={props.addComment} 
+                        postComment={props.postComment} 
                         campsiteId={props.campsite.id}
                     />
 
@@ -137,7 +137,7 @@ class CommentForm extends Component {
     }
     handleSubmit(values) {
         this.toggleModal();
-        this.props.addComment(this.props.campsiteId,values.rating,values.author,values.text);
+        this.props.postComment(this.props.campsiteId,values.rating,values.author,values.text);
         }
 
     render() {
